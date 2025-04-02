cdede000a3c19b9c1b86fbff278be3d78ee19f78
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-04-02 08:27:12 -0300

    Quarta 1st

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,5 +5,7 @@ class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria", )
+    list_per_page = 1
 
 admin.site.register(Fotografia, ListandoFotografias)
