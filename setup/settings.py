e3a11c9617791a35a96a5d13a6be8e427bf11221
rodrigo.resque <rodrigo.resque@tjpa.jus.br>
2025-05-08 09:56:32 -0300

    Updt

--- setup/settings.py
+++ setup/settings.py
@@ -131,3 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = "/media/"
